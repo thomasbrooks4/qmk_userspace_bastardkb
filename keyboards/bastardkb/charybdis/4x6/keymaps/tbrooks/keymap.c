5e18e0ce3dd0684a14e3a4fcb596dbd61cb444d7
Thomas Brooks <thomasobrooks@gmail.com>
2025-09-28 18:13:47 -0700

    Decreasing threshold to 0 and removing auto sniping

--- keyboards/bastardkb/charybdis/4x6/keymaps/tbrooks/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/tbrooks/keymap.c
@@ -27,8 +27,7 @@ enum charybdis_keymap_layers {
     LAYER_POINTER,
 };
 
-/** \brief Automatically enable sniping-mode on the pointer layer. */
-#define CHARYBDIS_AUTO_SNIPING_ON_LAYER LAYER_POINTER
+#define LAYER_POINTER
 
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 static uint16_t auto_pointer_layer_timer = 0;

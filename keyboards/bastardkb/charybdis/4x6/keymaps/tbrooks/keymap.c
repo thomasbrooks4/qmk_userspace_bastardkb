62cf139838c09fa453023ffbbf28df3398b8f156
Thomas Brooks <thomasobrooks@gmail.com>
2025-09-28 17:30:46 -0700

    Fixing shift keycode

--- keyboards/bastardkb/charybdis/4x6/keymaps/tbrooks/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/tbrooks/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,       KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, KC_LALT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                   KC_LGUI, KC_SPC,   LOWER,      RAISE, KC_LSHFT,
+                                  KC_LGUI,  KC_SPC,   LOWER,      RAISE, KC_LSFT,
                                            KC_LALT, KC_BSPC,     KC_ENT
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
